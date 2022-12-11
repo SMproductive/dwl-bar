88305ac9dac87a970657c9b424ed2b6c7727bf63
Maximilian Strele <SMproductive@tutanota.de>
2022-12-10 20:50:01 -0500

    test

--- dwl-bar.c
+++ dwl-bar.c
@@ -6,11 +6,14 @@ static void activate(GtkApplication *app, gpointer user_data) {
 
   window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "dwl-bar");
-  gtk_window_set_default_size(GTK_WINDOW(window), 1080, 100);
+  gtk_window_fullscreen(window);
+  gtk_window_set_default_size(window, gtk_widget_get_width(window), 100);
+  gtk_widget_set_size_request(window, gtk_widget_get_width(window), 100);
+  gtk_window_set_resizable(window, false);
+
   gtk_widget_show(window);
 
   bar = gtk_label_new("label");
-  gtk_widget_set_size_request(&bar, 1080, 100);
   gtk_window_set_child(GTK_WINDOW(window), bar);
 
   gtk_window_present(GTK_WINDOW(window));
