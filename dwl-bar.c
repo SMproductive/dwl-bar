4fc530435f901980e44f00a7a1d17babca0fe150
Maximilian Strele <SMproductive@tutanota.de>
2022-12-10 20:08:01 -0500

    test

--- dwl-bar.c
+++ dwl-bar.c
@@ -10,6 +10,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
   gtk_widget_show(window);
 
   bar = gtk_label_new("label");
+  gtk_widget_set_size_request(&bar, 1080, 100);
   gtk_window_set_child(GTK_WINDOW(window), bar);
 
   gtk_window_present(GTK_WINDOW(window));
@@ -19,10 +20,17 @@ int main(int argc, char **argv) {
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  app = gtk_application_new("dwl.bar", G_APPLICATION_FLAGS_NONE);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
 
   return status;
 }
+
+/* TODO
+ * Handle DWL string
+ * FIFO
+ * Window top
+ * Window size
+ * Style */
